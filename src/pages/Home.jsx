54d2f54edb3bbd0ae40055f0756dda406de649a6
Medichelme Harshith <medichelmharshith6@gmail.com>
2026-08-24 18:46:15 +0530

    Added credits

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,15 +8,26 @@ import Skills from "../components/Skills";
 
 const Home = () => {
   return (
-    <div className="selection:bg-black selection:text-white" >
+    <div className="selection:bg-black selection:text-white">
       <Header/>
       <Hero id="Me"/>
       <AboutMe id="About"/>
-     <Skills id="Skills"/>
+      <Skills id="Skills"/>
       <Projects id="Work"/>
       <Contact id="Connect"/>
+      <p className="text-center text-xs text-zinc-400 py-6">
+        Inspired by{" "}
+        <a
+          href="https://www.gowthamoleti.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="underline underline-offset-2 hover:text-zinc-600 transition-colors"
+        >
+          Gautham Oleti
+        </a>
+      </p>
     </div>
-  )
-}
+  );
+};
 
 export default Home;
